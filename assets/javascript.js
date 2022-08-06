0c87cff76cbba961981f27dc1cd327808907abcb
Knock <enoch10evelyn@gmail.com>
2022-08-06 12:23:31 -0700

    JS script is working added event listener to button

---
+++ assets/javascript.js
@@ -0,0 +1,10 @@
+//var time = $("<p/>");
+//button test to see if script works, still need to add CDN link for moment.js 
+var saveButton =$(".save-btn")
+saveButton.click(function(e){
+    alert("Your JS script works"); 
+});
+
+
+//time.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+//time.append($(".time-container"))
