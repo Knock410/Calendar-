df637a06926f51d2b4c89b758ac5aa09737d7743
Knock <enoch10evelyn@gmail.com>
2022-08-06 18:55:12 -0700

    Got current time to display and update

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,10 +1,25 @@
-//var time = $("<p/>");
+var time = $(".time-container");
 //button test to see if script works, still need to add CDN link for moment.js 
 var saveButton =$(".save-btn")
+
+time.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
 saveButton.click(function(e){
     alert("Your JS script works"); 
 });
 
+var block = getElementById('event-blocks');
 
-//time.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-//time.append($(".time-container"))
+console.log(block);
+//  var loadTasks = function() {
+//     tasks = JSON.parse(localStorage.getItem("tasks"));
+  
+//     // if nothing in localStorage, create a new object to track all task status arrays
+//     if (!tasks) {
+//       tasks = {
+//         toDo: [],
+//         inProgress: [],
+//         inReview: [],
+//         done: []
+//       };
+//     }
