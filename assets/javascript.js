850dbd4c5cf7fb7155acaffde9aa6e6cf9c7f49f
Knock <enoch10evelyn@gmail.com>
2022-08-25 19:38:42 -0700

    Multiple Changes to html, javascript and css

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,14 +1,73 @@
-var time = $(".time-container");
-//button test to see if script works, still need to add CDN link for moment.js 
-var saveButton =$(".save-btn")
+
+var time = $(".timeContainer");
+
+var saveButton =$(".saveBtn")
+
+var textAreas = document.querySelectorAll(".backgroundGizmo");
 
 time.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-saveButton.click(function(e){
-    alert("Your JS script works"); 
+var currentTime = parseInt(moment().format('H'));
+var hourPast = hourPast < currentTime; 
+var hourNow = currentTime;
+var hourFuture = hourFuture < currentTime;
+console.log(currentTime);
+
+//How to console log text 
+console.log(hourNow.text);
+console.log(time.text); 
+  function backgroundColorChange(){
+    if (hourNow == currentTime){
+        document.getElementsByClassName("backgroundGizmo").style.backgroundColor = "#c00000";
+    }else if (hourNow < currentTime){
+        document.getElementsByClassName("backgroundGizmo").style.backgroundColor = "#6161fb";
+    }else 
+    {
+        document.getElementsByClassName("backgroundGizmo").style.backgroundColor = "#90ee90";
+    }
+}
+//get value from local storage. ThisAreaH.textconent Localstorage.getitem i
+for(var i=0; i < textAreas.length; i++ ){
+    var thisTextAreaH = textAreas[i]
+    var thisHour = parseInt(thisTextAreaH.dataset.hour)
+    console.log(thisHour);
+     if(thisHour == currentTime){
+      thisTextAreaH.classList.add("present");  
+     } else if (thisHour < currentTime){
+       
+        thisTextAreaH.classList.add("past"); 
+     } else {
+        thisTextAreaH.classList.add("future"); 
+     }
+}
+
+
+// textAreas.each(function(i) {
+//  console.log($(this));
+//     var textArea = $(this)
+//     var thisBlockHour = textArea.getAttribute("data-hour");
+//    console.log(thisBlockHour);
+//    console.log(typeof(thisBlockHour));
+// } );
+
+
+
+saveButton.each(function(i) {
+    $(this).on("click", function() {
+        let thisHourText = $(this).prev();
+        console.log(thisHourText);
+        localStorage.setItem(`Event #${i+1}`, thisHourText.val());
+    });
 });
 
-var block = getElementById('event-blocks');
+
+//
+// saveButton.click(function(e){
+//     alert("Your JS script works"); 
+// });
+
+
+var block =document.getElementById('event-blocks');
 
 console.log(block);
 //  var loadTasks = function() {
